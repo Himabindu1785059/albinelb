7a109de76ab8b0b2bef2394711a4d8abe72f40a8
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-31 17:54:02 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -10,14 +10,14 @@ module "ec2" {
   my_public_key  = "/tmp/id_rsa.pub"
   instance_type  = "t2.micro"
   security_group = "${module.vpc.security_group}"
-  subnets        = "${module.vpc.public_subnets}"
+  subnets        = "${module.vpc.public_subnet}"
 }
 
 module "alb" {
-  source = "./alb"
-  vpc_id = "${module.vpc.vpc_id}"
+  source       = "./alb"
+  vpc_id       = "${module.vpc.vpc_id}"
   instance1_id = "${module.ec2.instance1_id}"
   instance2_id = "${module.ec2.instance2_id}"
-  subnet1 = "${module.vpc.subnet1}"
-  subnet2 = "${module.vpc.subnet2}"
+  subnet1      = "${module.vpc.subnet1}"
+  subnet2      = "${module.vpc.subnet2}"
 }
